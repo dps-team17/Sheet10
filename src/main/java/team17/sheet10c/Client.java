fd2531fa52313fee675c1d641b07cb35db7caa49
DaniWi <daniel.witsch@student.uibk.ac.at>
2016-05-28 10:49:04 +0200

    finsih hybrid

--- src/main/java/team17/sheet10c/Client.java
+++ src/main/java/team17/sheet10c/Client.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.Socket;
 import java.security.Key;
 
+import team17.sheet10a.EncryptedMessenger;
+import team17.sheet10a.EncryptedSocketConnection;
 import team17.sheet10b.EncryptedSocketConnectionRSA;
 import team17.sheet10b.RSAMain;
 
@@ -14,23 +16,38 @@ public class Client {
 
 	private String secretKey;
 
-	public Client(String privKeyFileName, String pubKeyFilename) {
+	private String host = "127.0.0.1";
+	private int portRSA = Server.SERVICE_PORT_RSA;
+	private int portSecret = Server.SERVICE_PORT_SECRET;
+
+	public Client(String privKeyFileName, String pubKeyFilename, String secretKey) {
 		privateKeyClient = RSAMain.readPrivateKeyFromFile(privKeyFileName);
 		publicKeyServer = RSAMain.readPublicKeyFromFile(pubKeyFilename);
+		this.secretKey = secretKey;
 	}
 
 	public void startRSA() {
-
-		String host = "127.0.0.1";
-		int port = Server.SERVICE_PORT;
-
 		try {
-			Socket socket = new Socket(host, port);
+			Socket socket = new Socket(host, portRSA);
 			EncryptedSocketConnectionRSA connection = new EncryptedSocketConnectionRSA(socket, publicKeyServer,
 					privateKeyClient);
 			connection.showEncryptedMessages(false);
-			secretKey = connection.receive();
-			System.out.println(secretKey);
+			connection.send(secretKey);
+			connection.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void startSecretConnection() {
+		try {
+			Socket socket = new Socket(host, portSecret);
+			EncryptedSocketConnection connection = new EncryptedSocketConnection(socket, secretKey);
+			connection.showEncryptedMessages(false);
+			EncryptedMessenger messenger = new EncryptedMessenger(connection);
+
+			messenger.start();
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -38,14 +55,18 @@ public class Client {
 	}
 
 	public static void main(String[] args) {
-		Client client = new Client("privateClient.key", "publicServer.key");
-		client.startRSA();
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (args.length != 1) {
+			System.err.println("Invalid parameter count! Expected Key.");
+			System.exit(1);
 		}
+
+		String secretKey = args[0];
+
+		Client client = new Client("privateClient.key", "publicServer.key", secretKey);
+		System.out.println("Send secret key!");
+		client.startRSA();
+		System.out.println("Secret connection start:");
+		client.startSecretConnection();
 	}
 
 }
--- src/main/java/team17/sheet10c/Server.java
+++ src/main/java/team17/sheet10c/Server.java
@@ -5,18 +5,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.Key;
 
+import team17.sheet10a.EncryptedMessenger;
+import team17.sheet10a.EncryptedSocketConnection;
 import team17.sheet10b.EncryptedSocketConnectionRSA;
 import team17.sheet10b.RSAMain;
 
 public class Server {
-	public static final int SERVICE_PORT = 12346;
+	public static final int SERVICE_PORT_RSA = 55555;
+	public static final int SERVICE_PORT_SECRET = 55556;
 
 	private Key privateKeyServer;
 	private Key publicKeyClient;
 
-	String secretKey;
+	private String secretKey;
 
-	public Server(String privKeyFileName, String pubKeyFilename, String secretKey) {
+	public Server(String privKeyFileName, String pubKeyFilename) {
 		privateKeyServer = RSAMain.readPrivateKeyFromFile(privKeyFileName);
 		publicKeyClient = RSAMain.readPublicKeyFromFile(pubKeyFilename);
 	}
@@ -25,16 +28,37 @@ public class Server {
 
 		Socket clientSocket;
 
-		try (ServerSocket serverSocket = new ServerSocket(SERVICE_PORT)) {
+		try (ServerSocket serverSocket = new ServerSocket(SERVICE_PORT_RSA)) {
 
-			System.out.println("Server started. Listening on port " + SERVICE_PORT);
+			System.out.println("Server started. Listening on port " + SERVICE_PORT_RSA);
+
+			clientSocket = serverSocket.accept();
+
+			EncryptedSocketConnectionRSA connection = new EncryptedSocketConnectionRSA(clientSocket, publicKeyClient,
+					privateKeyServer);
+			connection.showEncryptedMessages(false);
+			secretKey = connection.receive();
+			connection.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void startSecretConnection() {
+		Socket clientSocket;
+
+		try (ServerSocket serverSocket = new ServerSocket(SERVICE_PORT_SECRET)) {
+
+			System.out.println("Server started. Listening on port " + SERVICE_PORT_SECRET);
 
 			while ((clientSocket = serverSocket.accept()) != null) {
 
-				EncryptedSocketConnectionRSA connection = new EncryptedSocketConnectionRSA(clientSocket,
-						publicKeyClient, privateKeyServer);
+				EncryptedSocketConnection connection = new EncryptedSocketConnection(clientSocket, secretKey);
 				connection.showEncryptedMessages(false);
-				connection.send(secretKey);
+				EncryptedMessenger messenger = new EncryptedMessenger(connection);
+
+				messenger.start();
 			}
 
 		} catch (IOException e) {
@@ -43,14 +67,11 @@ public class Server {
 	}
 
 	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.err.println("Invalid parameter count! Expected Key.");
-			System.exit(1);
-		}
-
-		String secretKey = args[0];
-		Server server = new Server("privateServer.key", "publicClient.key", secretKey);
+		Server server = new Server("privateServer.key", "publicClient.key");
+		System.out.println("Get secret key!");
 		server.start();
+		System.out.println("Secret connection start:");
+		server.startSecretConnection();
 	}
 
 }
